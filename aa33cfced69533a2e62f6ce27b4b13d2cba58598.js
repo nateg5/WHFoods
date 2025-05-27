a944a024fb1ccf66557cd4f14d8fb672d0e5dba5
Nate Gardner <nateg5@gmail.com>
2025-05-27 11:25:22 -0500

    - initial commit of whfood flattened data from webarchive

---
+++ aa33cfced69533a2e62f6ce27b4b13d2cba58598.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+
+}
+/*
+     FILE ARCHIVED ON 18:11:01 Sep 26, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:52:44 May 20, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.69
+  exclusion.robots: 0.033
+  exclusion.robots.policy: 0.019
+  esindex: 0.015
+  cdx.remote: 35.168
+  LoadShardBlock: 2392.412 (3)
+  PetaboxLoader3.datanode: 2299.347 (4)
+  PetaboxLoader3.resolve: 153.311 (3)
+  load_resource: 121.444
+*/
